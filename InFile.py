7e9b6ef5aab5810b197af50bcf68fc27a3c1dc42
jason <lkarry0199@Gmail.com>
2022-11-15 14:43:26 +0800

    Initial Project Structure

---
+++ InFile.py
@@ -0,0 +1,9 @@
+class InFile:
+
+    def __int__(self, path):
+        self.folderPath = path
+        self.inType = []
+        self.inRange = []
+
+    def parse_stdin(self):
+        ...
---
+++ Judger.py
@@ -0,0 +1,9 @@
+class Judger:
+    def __int__(self, infile):
+        self.infile = infile
+
+    def judge(self):
+        ...
+
+    def write_out(self):
+        ...
